b1f6c8485bc7d912f2e138da825d5751a1e63a6f
Suspecatt <joaooluiz443@gmail.com>
2022-07-06 16:30:35 -0300

    Aula 3 - Adiciona metodos novos em financiamento e parcela

--- financiamento.js
+++ financiamento.js
@@ -9,4 +9,24 @@ class financiamento {
         this.#prazo = prazo
         this.#parcelas.push(new parcela(0, 0, 0, 0, valor - entrada))
     }
+    static calcula_juros(valor, taxa_juros) {
+        return valor * (taxa_juros / 100)
+    }
+    calcula_parcelas_mensais() {
+        let saldo = this.#parcelas[this.#parcelas.length - 1].get_saldo()
+        let prazo = this.prazo - (this.#parcelas - 1)
+        let amortizacao = saldo / prazo
+        for (i = 0; i < prazo; i++) {
+            const numero = this.#parcelas.length
+            const juros = financiamento.calcula_juros(saldo, this.#taxa_juros)
+            const valor = juros - amortizacao
+            saldo -= amortizacao
+            if (saldo < 0) {
+                saldo = 0
+            }
+            this.#parcelas.push(
+                new parcela(numero, valor, juros, amortizacao, saldo)
+            )
+        }
+    }
 }
--- parcela.js
+++ parcela.js
@@ -11,4 +11,7 @@ export class parcela {
         this.#amortizacao = amortizacao
         this.#saldo = saldo
     }
+    get_saldo () {
+        return this.#saldo
+    }
 }
